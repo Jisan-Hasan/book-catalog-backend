5c01f540916643129be74aeda5d8f04b7b4b6fc6
Jisan-Hasan <jisan.jh5@gmail.com>
2023-09-23 13:52:31 +0600

    Book CREATE API created

---
+++ src/app/modules/book/book.route.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { BookController } from './book.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+
+const router = express.Router();
+
+router.post(
+  '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
+  BookController.insertIntoDB
+);
+
+export const BookRoutes = router;
---
+++ src/app/modules/book/book.service.ts
@@ -0,0 +1,16 @@
+import { Book } from '@prisma/client';
+import prisma from '../../../shared/prisma';
+
+const insertIntoDB = async (payload: Book): Promise<Book> => {
+  const result = await prisma.book.create({
+    data: payload,
+    include: {
+      category: true,
+    },
+  });
+  return result;
+};
+
+export const BookService = {
+  insertIntoDB,
+};
